b99f334f01a13c1fb2b79aebd13c55e77bf3d7c8
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-11-17 21:10:04 +0100

    feat: add favicon and change page title

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bulma/css/bulma.css';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { AppProvider } from './context/AppContext';
